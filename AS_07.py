b592fee5e6a7d9d1c7dea26337dbd1cd4b1d3668
Richard Hoehn <rhoehn@gmail.com>
2023-04-05 16:18:59 -0500

    Create AS_07.py

---
+++ AS_07.py
@@ -0,0 +1,10 @@
+# University:  Middle Tennessee State University - MTSU
+# Class:       CSCI 7400 - Assignment 07
+# By:          Richard Hoehn (PhD Student)
+# Due Date:    2023-04-21
+# Desc:        This assignment is to learn and demonstrate student understanding of some basic spark operations including Transformations and Actions.
+# Links:       * Spark:     https://spark.apache.org/docs/3.1.3/api/python/index.html
+#              * GIT:       https://github.com/richardhoehn/mtsu.csci.7400.assignment07
+#.             * Data:      https://archive.ics.uci.edu/ml/datasets/Sentiment+Labelled+Sentences
+
+from pyspark.sql import SparkSession
